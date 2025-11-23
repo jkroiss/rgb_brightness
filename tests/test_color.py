d56e1e7db8a17a604665eb9e320af124b71502e3
jk <krs.jul@gmail.com>
2025-11-23 12:52:28 +0100

    Add empty tests for color class

---
+++ tests/test_color.py
@@ -0,0 +1,25 @@
+import unittest
+import math
+from src.color import Color
+
+class TestColor(unittest.TestCase):
+    
+    def test_parse_full_hex_string(self):
+        pass
+
+    def pass_short_hext_string(self):
+        pass
+
+    def test_invalid_hey_string(self):
+        pass
+
+    def test_brightness_calculation(self):
+        pass
+
+    def test_optional_attributes(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
