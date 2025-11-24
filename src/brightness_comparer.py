bb19454e9381a4a4e964f19ff00b1fa80f6f7853
jk <krs.jul@gmail.com>
2025-11-24 12:21:19 +0100

    change docstring-style and added missing docstrings to the functions

--- src/brightness_comparer.py
+++ src/brightness_comparer.py
@@ -2,16 +2,16 @@ from typing import List
 from src.color import Color
 
 class BrightnessComparer:
-    '''
+    """
     Finds the highest brightness of Color-objects in a List.
 
     Additionally, it provides functionality to print the brightest color directly and 
     to format the found object into a proper human-readable string, if so desired.
-    '''
+    """
 
     @staticmethod
     def get_brightest_color(colors: List[Color]):
-        
+        """Function that finds the brightest color in a list of Color-Objects."""
         if not colors:
             raise ValueError("No list of colors provided.")
 
@@ -19,6 +19,7 @@ class BrightnessComparer:
 
     @staticmethod
     def format_brightest_color(color: Color) -> str:
+        """Function that formats the brightest Color-object into a human-readable output string"""
         hex_string = color.hex_string
         if not hex_string.startswith('#'):
             hex_string = '#' + hex_string
@@ -30,6 +31,7 @@ class BrightnessComparer:
         
     @staticmethod
     def print_brightest_color(colors):
+        """Function that finds and prints the brightest Color-object in a list of Color-objects."""
         brightest = BrightnessComparer.get_brightest_color(colors)
         output = BrightnessComparer.format_brightest_color(brightest)
         print(output)
